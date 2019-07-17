2a697f5f0d45cf7e954bfb59c5774808f7832575
dolanskurd <dolanskurd@mail.com>
2019-07-17 16:22:47 +0300

    providing a new updated list and newfile including 7169 sentences. Thanks to Wikipedia and Python

--- corpus/dup.py
+++ corpus/dup.py
@@ -1,4 +1,4 @@
-with open("kurdi_list_UP.txt", "r", encoding="utf-8") as f:
+with open("kurdi_words.txt", "r", encoding="utf-8") as f:
     new = []
     for i in f:
         if not i in new:
@@ -7,5 +7,5 @@ with open("kurdi_list_UP.txt", "r", encoding="utf-8") as f:
             pass
 new.sort()
 for i in new:
-    with open("newlist_without_dup_01.txt", "a", encoding="utf-8") as f:
+    with open("newlist_sentence.txt", "a", encoding="utf-8") as f:
         f.write(i)
